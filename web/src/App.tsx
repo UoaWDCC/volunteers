503854f104f7df59a99bb22167dbeafeef8fa4ae
hgao080 <hgao080@aucklanduni.ac.nz>
2024-04-11 09:38:58 +1200

    Removed path to '/:name'

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { useState } from 'react';
-import Home from '@pages/Home';
 import MainMenu from '@pages/MainMenu';
 import NotFound from '@pages/NotFound';
 
 const router = createBrowserRouter([
-  {
-    path: '/:name',
-    element: <Home />,
-  },
   {
     path: '/',
     element: <MainMenu />,
