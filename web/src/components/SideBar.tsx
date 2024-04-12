b4a4a4d8562a96c8fa62b04ec36443716a837988
Taks07 <yutakaoishi7@gmail.com>
2024-04-12 19:00:52 +1200

    Anchor changed to react route Link tags. Each page link is wrapped in their own Link tag

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../styles/componentStyles/SideBar.css';
 
 const SideBar = () => {
@@ -6,32 +7,40 @@ const SideBar = () => {
             <img className='club-logo' src='src/assets/club-logo.svg' alt='club-logo' />
 
             <div className="sidebar-links">
-                <a href="#">
+                <Link to='/'>
                     <div className="selected">
                         <div className='image-div'><img src='src/assets/sidebar/dashboard-icon.svg' alt='dashboard-icon' /></div>
                         <h3>Dashboard</h3>
                     </div>
+                </Link>
 
+                <Link to='/'>
                     <div className="">
                         <div className='image-div'><img src='src/assets/sidebar/discover-icon.svg' alt='discover-icon' /></div>
                         <h3>Discover</h3>
                     </div>
+                </Link>
 
+                <Link to='/'>
                     <div className="">
                         <div className='image-div'><img src='src/assets/sidebar/profile-icon.svg' alt='profile-icon' /></div>
                         <h3>My Profile</h3>
                     </div>
+                </Link>
 
+                <Link to='/'>
                     <div className="">
                         <div className='image-div'><img src='src/assets/sidebar/social-icon.svg' alt='social-icon' /></div>
                         <h3>Social</h3>
                     </div>
+                </Link>
 
+                <Link to='/'>
                     <div className="">
                         <div className='image-div'><img src='src/assets/sidebar/settings-icon.svg' alt='settings-icon' /></div>
                         <h3>Settings</h3>
                     </div>
-                </a>
+                </Link>
             </div>
         </div>
      );
