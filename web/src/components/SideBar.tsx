2ebce51cf9ef933b8c52f1f709a732bc4042544f
Taks07 <yutakaoishi7@gmail.com>
2024-04-12 17:38:14 +1200

    Added club logo and its css

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import '../styles/componentStyles/SideBar.css';
 const SideBar = () => {
     return ( 
         <div className="sidebar">
-            
+            <img className='club-logo' src='src/assets/club-logo.svg' alt='club-logo' />
         </div>
      );
 }
