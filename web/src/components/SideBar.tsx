a392efd49edb50b3f8c16fec2bce59fc482aba61
Taks07 <yutakaoishi7@gmail.com>
2024-04-13 00:54:10 +1200

    Moved location of Link tags for each link. Changed css so anywhere in tab can be clicked in order to select page. Tabs now rounded

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -7,40 +7,42 @@ const SideBar = () => {
             <img className='club-logo' src='src/assets/club-logo.svg' alt='club-logo' />
 
             <div className="sidebar-links">
-                <Link to='/'>
-                    <div className="selected">
+                <div className="selected">
+                    <Link to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/dashboard-icon.svg' alt='dashboard-icon' /></div>
                         <h3>Dashboard</h3>
-                    </div>
-                </Link>
-
-                <Link to='/'>
-                    <div className="">
+                    </Link>
+                </div>
+            
+                <div className="">
+                    <Link to='/discover'>
                         <div className='image-div'><img src='src/assets/sidebar/discover-icon.svg' alt='discover-icon' /></div>
                         <h3>Discover</h3>
-                    </div>
-                </Link>
-
-                <Link to='/'>
-                    <div className="">
+                    </Link>
+                </div>
+            
+                <div className="">
+                    <Link to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/profile-icon.svg' alt='profile-icon' /></div>
                         <h3>My Profile</h3>
-                    </div>
-                </Link>
-
-                <Link to='/'>
-                    <div className="">
+                    </Link>
+                </div>
+        
+                <div className="">
+                    <Link to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/social-icon.svg' alt='social-icon' /></div>
                         <h3>Social</h3>
-                    </div>
-                </Link>
-
-                <Link to='/'>
-                    <div className="">
+                    </Link>
+                </div>
+                
+                
+                <div className="">
+                    <Link to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/settings-icon.svg' alt='settings-icon' /></div>
                         <h3>Settings</h3>
-                    </div>
-                </Link>
+                    </Link>
+                </div>
+                
             </div>
         </div>
      );
