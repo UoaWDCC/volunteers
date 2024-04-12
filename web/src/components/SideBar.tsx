d58fef7c01c2c904e5fe5404c7d36fbaefae8ebd
Taks07 <yutakaoishi7@gmail.com>
2024-04-12 17:12:17 +1200

    Added basic css for sidebar background, size and shape

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -1,7 +1,9 @@
+import '../styles/componentStyles/SideBar.css';
+
 const SideBar = () => {
     return ( 
         <div className="sidebar">
-            <h2>SideBar</h2>
+            
         </div>
      );
 }
