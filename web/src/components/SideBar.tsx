9744a62c44ed04f29351a4db3bfd144e9f42002c
Taks07 <yutakaoishi7@gmail.com>
2024-04-13 01:41:46 +1200

    Added interactivity of clicking tabs and them changing color

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -1,43 +1,47 @@
 import { Link } from 'react-router-dom';
 import '../styles/componentStyles/SideBar.css';
+import { useState } from 'react';
 
 const SideBar = () => {
+    
+    const [selectedTab, setSelectedTab] = useState('dashboard');
+
     return ( 
         <div className="sidebar">
             <img className='club-logo' src='src/assets/club-logo.svg' alt='club-logo' />
 
             <div className="sidebar-links">
-                <div className="selected">
-                    <Link to='/'>
+                <div className={selectedTab === "dashboard" ? "selected" : "" }>
+                    <Link onClick={() => setSelectedTab("dashboard")} to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/dashboard-icon.svg' alt='dashboard-icon' /></div>
                         <h3>Dashboard</h3>
                     </Link>
                 </div>
             
-                <div className="">
-                    <Link to='/'>
+                <div className={selectedTab === "discover" ? "selected" : "" }>
+                    <Link onClick={() => setSelectedTab("discover")} to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/discover-icon.svg' alt='discover-icon' /></div>
                         <h3>Discover</h3>
                     </Link>
                 </div>
             
-                <div className="">
-                    <Link to='/'>
+                <div className={selectedTab === "profile" ? "selected" : "" }>
+                    <Link onClick={() => setSelectedTab("profile")} to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/profile-icon.svg' alt='profile-icon' /></div>
                         <h3>My Profile</h3>
                     </Link>
                 </div>
         
-                <div className="">
-                    <Link to='/'>
+                <div className={selectedTab === "social" ? "selected" : "" }>
+                    <Link onClick={() => setSelectedTab("social")} to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/social-icon.svg' alt='social-icon' /></div>
                         <h3>Social</h3>
                     </Link>
                 </div>
                 
                 
-                <div className="">
-                    <Link to='/'>
+                <div className={selectedTab === "settings" ? "selected" : "" }>
+                    <Link onClick={() => setSelectedTab("settings")} to='/'>
                         <div className='image-div'><img src='src/assets/sidebar/settings-icon.svg' alt='settings-icon' /></div>
                         <h3>Settings</h3>
                     </Link>
