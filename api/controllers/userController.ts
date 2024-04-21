87969764555153bec14084c7f23aeda2324fe0c0
Taks07 <yutakaoishi7@gmail.com>
2024-04-22 00:26:41 +1200

    Created userController to provide functions for requests involving user collection

---
+++ api/controllers/userController.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { db } from "../config/firebase";
+import { config } from 'dotenv';
+
+config();
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+}
+
