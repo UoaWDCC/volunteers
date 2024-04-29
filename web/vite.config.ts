78d93c92117936bb3ac5585bc3f14a1be493c17e
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-04-30 11:43:17 +1200

    relative path, and dev proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
   resolve: {
     alias: {
       '@utils': '/src/utils',
@@ -11,6 +13,18 @@ export default defineConfig({
       '@pages': '/src/pages',
       '@contexts': '/src/contexts',
       '@layouts': '/src/layouts',
+    },
+  },
+
+  server: {
+    proxy: {
+      // Proxy configuration here
+      // Example:
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 });
