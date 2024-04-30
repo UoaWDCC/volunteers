a8fa0c9e4c7370a005f023194ef009d14a6b4213
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-04-30 12:30:28 +1200

    proxy url

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  
 
   resolve: {
     alias: {
@@ -18,13 +18,7 @@ export default defineConfig({
 
   server: {
     proxy: {
-      // Proxy configuration here
-      // Example:
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      '/api': "http://localhost:3000",
     },
   },
 });
