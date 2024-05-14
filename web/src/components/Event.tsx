fa8023b54a4d37055a1f38d3cd3191bbf794afd1
dis-may <maygan275@gmail.com>
2024-05-14 12:19:29 +1200

    Changed event component styles to Tailwind, deleted corresponding Events.css file

--- web/src/components/Event.tsx
+++ web/src/components/Event.tsx
@@ -2,21 +2,23 @@ import "../styles/componentStyles/Event.css"
 
 export default function Event() {
     return (
-        <div className="container">
-            <div className="image-container">
-                <img className="event-image" src="src/assets/gallery/events/sample1.png" />
+        <div className="flex bg-slate-50 items-center"> {/* event-container */}
+            <div className="inline m-3"> {/* image-container */}
+                <img className="h-28 w-40 object-cover rounded-md" src="src/assets/gallery/events/sample1.png" />
             </div>
-            <div className="text-container">
-                <p className="date">SAT, 11 MAY</p>
-                <h2 className="event-title">UoA Annual Volunteers Day</h2>
-                <p className="location">The University of Auckland</p>
-                <div className="people">
-                    <img className="profile-image" src="src/assets/gallery/events/sample1.png" />
-                    <p className="inline-text">Eduardo is interested</p>
+            <div className="inline p-1.5"> {/* event details on the right */}
+                <p className="mb-0 font-bold text-xs">SAT, 11 MAY</p>
+                <h2 className="inline font-bold m-0 text-xl">UoA Annual Volunteers Day</h2>
+                <p className="mb-0">The University of Auckland</p>
+                <div className="inline-block items-center py-1 pr-5"> {/* people */}
+                    {/* profile image */}
+                    <img className="h-7 w-7 object-cover rounded-full inline" src="src/assets/gallery/events/sample1.png" />
+                    <p className="inline ml-2.5 text-sm">Eduardo is interested</p>
                 </div>
-                <div className="people">
-                    <img className="profile-image" src="src/assets/gallery/events/sample1.png" />
-                    <img className="profile-image-special" src="src/assets/gallery/events/sample1.png" />
+                <div className="inline-block items-center py-1 pr-5"> {/* people */}
+                    {/* profile image */}
+                    <img className="h-7 w-7 object-cover rounded-full inline" src="src/assets/gallery/events/sample1.png" />
+                    <img className="h-7 w-7 object-cover rounded-full inline -ml-2.5" src="src/assets/gallery/events/sample1.png" />
                     <p className="inline-text">John, May, and 4 other friends are going</p>
                 </div>
             </div>
