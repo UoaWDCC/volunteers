06272bf9dabd17b7720ca22f69565466c39f0ea0
dis-may <maygan275@gmail.com>
2024-05-14 12:38:20 +1200

    Updated file path for images

--- web/src/components/Event.tsx
+++ web/src/components/Event.tsx
@@ -2,7 +2,7 @@ export default function Event() {
     return (
         <div className="flex bg-slate-50 items-center m-2"> {/* event-container */}
             <div className="inline m-3"> {/* image-container */}
-                <img className="h-24 w-40 object-cover rounded-md" src="src/assets/gallery/events/sample1.png" />
+                <img className="h-24 w-40 object-cover rounded-md" src="../../public/assets/gallery/events/sample1.png" />
             </div>
             <div className="inline p-1.5"> {/* event details on the right */}
                 <p className="mb-0 font-bold text-xs">SAT, 11 MAY</p>
@@ -10,14 +10,14 @@ export default function Event() {
                 <p className="mb-0">The University of Auckland</p>
                 <div className="inline-block items-center py-1 pr-5"> {/* people */}
                     {/* profile image */}
-                    <img className="h-7 w-7 object-cover rounded-full inline" src="src/assets/gallery/events/sample1.png" />
+                    <img className="h-7 w-7 object-cover rounded-full inline" src="../../public/assets/gallery/events/sample1.png" />
                     <p className="inline ml-2.5 text-sm">Eduardo is interested</p>
                 </div>
                 <div className="inline-block items-center py-1 pr-5"> {/* people */}
                     {/* profile image */}
-                    <img className="h-7 w-7 object-cover rounded-full inline" src="src/assets/gallery/events/sample1.png" />
+                    <img className="h-7 w-7 object-cover rounded-full inline" src="../../public/assets/gallery/events/sample1.png" />
                     {/* profile image which overlaps */}
-                    <img className="h-7 w-7 object-cover rounded-full inline -ml-2.5" src="src/assets/gallery/events/sample1.png" />
+                    <img className="h-7 w-7 object-cover rounded-full inline -ml-2.5" src="../../public/assets/gallery/events/sample1.png" />
                     <p className="inline ml-2.5 text-sm">John, May, and 4 other friends are going</p>
                 </div>
             </div>
