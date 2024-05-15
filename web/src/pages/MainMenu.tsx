df642a5a452f44dc652b54efeace12a20ea4191d
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-05-15 18:45:06 +1200

    tailwindconfig

--- web/src/pages/MainMenu.tsx
+++ web/src/pages/MainMenu.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Header from '@components/Header';
 import Footer from '../components/Footer';
 import '../styles/pageStyles/MainMenu.css';
@@ -11,34 +10,10 @@ import { eventHighlights } from '../data/EventHighlights.json';
 import OurAchievements from '@components/OurAchievements';
 import OurCommunity from '@components/OurCommunity';
 function MainMenu() {
-  const [testList, setTestList] = useState<{ id: string, name: string, number: number }[]>([]);
-
-  useEffect(() => {
-    const fetchTestList = async () => {
-      try {
-        const response = await fetch('/api/getTest');
-        if (!response.ok) {
-          throw new Error('Failed to fetch test list');
-        }
-        const data = await response.json();
-        setTestList(data);
-      } catch (error) {
-        console.error("Error fetching test list:", error);
-      }
-    };
-
-    fetchTestList();
-  }, []);
 
   return (
-    <div className="MainMenu">
+    <div className="primary-background">
       <Header />
-      {/* Render testList data */}
-      <ul>
-        {testList.map((item) => (
-          <li key={item.id}>{item.name} - {item.number}</li>
-        ))}
-      </ul>
       <MainGallery data={events} />
       <EventHighlights data={eventHighlights} />
       <OurAchievements/>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,9 +8,43 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat'],
-        body: ['Open Sans'],
+        sans: ['Work Sans']
       },
+      fontSize: {
+        xlg: "37.46px", /*Welcome Back...*/
+        lg: "17.03px", /*Glad to see you're back...*/
+        md: "12.1px", /*Launch Night*/
+        sml: "7.26px", /*Monday 8th April...*/
+        xs: "4.98px" /*club Event*/
+      },
+      letterSpacing: {
+        
+      },
+      fontColor: {
+        primary: "#FFFFFF"
+      },
+      lineHeight: {
+        xlg: "43.94px", /*Welcome Back...*/
+        lg: "19.97px", /*Glad to see you're back...*/
+        md: "14.2px", /*Launch Night*/
+        sml: "8.52px", /*Monday 8th April...*/
+        xs: "5.84px" /*club Event*/
+      },
+      colors: {
+        primary: {
+          background: "#FFFEF9", /*white background*/
+          blue: "#3B87DD" /*main page bold blue + dashboard buttons etc*/
+        },
+        accent: {
+          lighter_blue: "#8BB4F9", /*About us button on main page*/
+        },
+        university: {
+          darkblue: "#255894" /*Classic UOA darker blue*/
+        },
+        dashboard: {
+          light_blue_gradient: "linear-gradient(180deg, #D6EDFD 0%, #58B6F1 100%)" /*Dashboard gradient*/
+        }
+      }
     },
   },
   daisyui: {
