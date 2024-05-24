1241bfa13b4f2b43828fb7d98ecf2059b398fb11
hgao080 <hgao080@aucklanduni.ac.nz>
2024-05-25 11:45:01 +1200

    Changed to Tailwind CSS. Added extra Register button and changed button design to match Figma

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,18 +1,17 @@
-import "../styles/componentStyles/Header.css";
 import { handleGoogle } from "../../services/firebase.tsx";
 
 function Header() {
   return (
-    <header className="header">
-      <div className="header-content">
-
-        <div className="header-left">
-          <img src="/public/assets/header-logo.svg" alt="Volunteers Club Logo" />
+    <header className="flex justify-center bg-neutral-tan">
+      <div className="flex flex-grow justify-between items-center min-w-min px-44 py-3">
+        <div>
+          <img src="/public/assets/header-logo.svg" alt="Volunteers Club Logo" className="h-auto min-w-[150px]"/>
         </div>
 
-        <div className="header-right">
-          <ul className="links">
-            <li onClick={ handleGoogle } className="button">Sign in</li>
+        <div>
+          <ul className="flex flex-row text-sm text-white font-mono m-0 gap-5">
+            <li onClick={ handleGoogle } className="px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer">Login</li>
+            <li className="px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer">Register</li>
           </ul>
         </div>
       </div>
