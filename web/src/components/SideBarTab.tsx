6d08532bc207c6d972b43cd11399e6d239e82cdd
Taks07 <yutakaoishi7@gmail.com>
2024-05-25 14:50:21 +1200

    Improved positioning of images within sidebar tabs

--- web/src/components/SideBarTab.tsx
+++ web/src/components/SideBarTab.tsx
@@ -12,7 +12,7 @@ const SideBarTab: React.FC<SideBarTabProps> = ({ image, tabName, selectedTab, se
 
     return (
         <Link className={selectedTab === tabName ? baseTabStyle + " bg-white text-[#5AB7F1]" : baseTabStyle + " hover:bg-[#ffffff40] text-white"} onClick={() => setSelectedTab(tabName)} to="/">
-            <div className="w-[65px] h-[65px] ml-[15px] mr-[-10px] mt-[20px]">
+            <div className="w-[65px] h-[65px] ml-[12px] mr-[-10px] mt-[22px]">
                 <img className="w-[40px] h-[40px]" src={image} alt={tabName + " icon"} />
             </div>
             <h3 className="h-[125px] w-[64px] content-center font-sans text-[17px] mb-0">{tabName.substring(0, 1).toUpperCase() + tabName.substring(1)}</h3>
