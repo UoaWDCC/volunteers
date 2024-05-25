a8e2d0cfb18ad1795a876f947eb022b1866349a2
Taks07 <yutakaoishi7@gmail.com>
2024-05-25 14:47:34 +1200

    Added mask to tabs, and fixed positioning

--- web/src/components/SideBarTab.tsx
+++ web/src/components/SideBarTab.tsx
@@ -8,14 +8,14 @@ type SideBarTabProps = {
 };
 
 const SideBarTab: React.FC<SideBarTabProps> = ({ image, tabName, selectedTab, setSelectedTab }) => {
-    const baseTabStyle = "flex flex-row items-center hover:no-underline w-[190px] h-[84px] my-[-5px] transition-all duration-[400]"
+    const baseTabStyle = "flex flex-row items-center hover:no-underline w-[190px] h-[84px] my-[-5px] transition-all duration-[400] mask-tab"
 
     return (
         <Link className={selectedTab === tabName ? baseTabStyle + " bg-white text-[#5AB7F1]" : baseTabStyle + " hover:bg-[#ffffff40] text-white"} onClick={() => setSelectedTab(tabName)} to="/">
-            <div className="w-[65px] h-[65px]">
-                <img className="w-[52px] h-[52px]" src={image} alt={tabName + " icon"} />
+            <div className="w-[65px] h-[65px] ml-[15px] mr-[-10px] mt-[20px]">
+                <img className="w-[40px] h-[40px]" src={image} alt={tabName + " icon"} />
             </div>
-            <h3 className="h-[125px] w-[64px] content-center font-sans text-[17px] mb-0">{tabName}</h3>
+            <h3 className="h-[125px] w-[64px] content-center font-sans text-[17px] mb-0">{tabName.substring(0, 1).toUpperCase() + tabName.substring(1)}</h3>
         </Link>
     );
 };
