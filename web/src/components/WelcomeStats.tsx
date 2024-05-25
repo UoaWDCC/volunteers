c5fa5d112b1032de62ee0aebacc2156ce1cdbed7
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2024-05-25 18:18:30 +1200

    coverting WelcomeStats.tsx to tailwind

--- web/src/components/WelcomeStats.tsx
+++ web/src/components/WelcomeStats.tsx
@@ -1,12 +1,11 @@
 // WelcomeStats
-import "../styles/componentStyles/WelcomeStats.css";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function WelcomeStats() {
   // future fetch data from the server. (User, Hours, Shifts, etc.)
   // =========== TEMPORARY: hard code data ===========
   const Data = {
-    User: "[placeholder]",
+    User: '[placeholder]',
     Hours: 40,
     Shifts: 5,
   };
@@ -15,25 +14,25 @@ function WelcomeStats() {
   // =========== TEMPORARY: hard code data ===========
 
   return (
-    <div className="hour-counter">
-      <div className="welcome-message">
-        <h1 className="heading1">
-          Welcome Back, <span className="user-on-new-line">{User}!</span>
+    <div className='theme-dashboard bg-white flex items-center rounded-[50px] px-[4%] py-[2%] w-[45%] gap-[2%]'>
+      <div className='text-gray mr-auto w-[50%] min-h-[0]'>
+        <h1 className='text-3xl'>
+          Welcome Back, <span className='user-on-new-line'>{User}!</span>
         </h1>
-        <h3 className="heading3">Glad to see you're back in action!</h3>
-        <Link className="see-more" to="/">
+        <h3 className='text-detail mb-[5%]'>Glad to see you're back in action!</h3>
+        <Link className='see-more bg-primary text-white text-[11px] m-0 px-[4%] py-[1.5%] rounded-[100px] hover:bg-primary-dark hover:no-underline' to='/'>
           See More
         </Link>
       </div>
 
-      <div className="my-stats">
-        <div className="hours">
-          <h2>{Hours}</h2>
-          <p>Hours</p>
+      <div className='text-gray justify-between flex gap-[5px] ml-auto w-[50%]'>
+        <div className='bg-gray-100 flex justify-around flex-col rounded-[30px] px-[15%] py-[5%] text-center items-center'>
+          <h2 className='m-0 mt-[20%]'>{Hours}</h2>
+          <p className='m-0 my-[20%] font-medium'>Hours</p>
         </div>
-        <div className="shifts">
-          <h2>{Shifts}</h2>
-          <p>Shifts</p>
+        <div className='bg-gray-100 flex justify-around flex-col rounded-[30px] px-[15%] py-[5%] text-center items-center'>
+          <h2 className='m-0 mt-[20%]'>{Shifts}</h2>
+          <p className='m-0 mb-[20%] font-medium'>Shifts</p>
         </div>
       </div>
     </div>
