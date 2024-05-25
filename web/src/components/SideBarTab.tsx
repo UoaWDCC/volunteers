1907603d4ed3bd96e982699d7466f1eceac999e4
Taks07 <yutakaoishi7@gmail.com>
2024-05-25 14:14:36 +1200

    Styling of tabs almost done except for mask shape and slight positioning issues

--- web/src/components/SideBarTab.tsx
+++ web/src/components/SideBarTab.tsx
@@ -8,15 +8,15 @@ type SideBarTabProps = {
 };
 
 const SideBarTab: React.FC<SideBarTabProps> = ({ image, tabName, selectedTab, setSelectedTab }) => {
+    const baseTabStyle = "flex flex-row items-center hover:no-underline w-[190px] h-[84px] my-[-5px] transition-all duration-[400]"
+
     return (
-        <div className={selectedTab === tabName ? "selected" : "unselected-tab"}>
-            <Link onClick={() => setSelectedTab(tabName)} to="/">
-                <div className="image-div">
-                    <img src={image} alt={tabName + " icon"} />
-                </div>
-                <h3>{tabName}</h3>
-            </Link>
-        </div>
+        <Link className={selectedTab === tabName ? baseTabStyle + " bg-white text-[#5AB7F1]" : baseTabStyle + " hover:bg-[#ffffff40] text-white"} onClick={() => setSelectedTab(tabName)} to="/">
+            <div className="w-[65px] h-[65px]">
+                <img className="w-[52px] h-[52px]" src={image} alt={tabName + " icon"} />
+            </div>
+            <h3 className="h-[125px] w-[64px] content-center text-[17px] mb-0">{tabName}</h3>
+        </Link>
     );
 };
  
