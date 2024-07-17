71e9b9d141967b02709b5ba2aab2fd211820d69f
hgao080 <hgao080@aucklanduni.ac.nz>
2024-07-17 18:07:15 +1200

    Added a filter by year level feature

--- web/src/pages/AdminUsers.tsx
+++ web/src/pages/AdminUsers.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import UserDetails from "@components/UserDetails";
 import { useUsersContext } from "../Hooks/UseUsersContext";
 import { User } from "../components/UserDetails"
@@ -6,7 +6,8 @@ import { User } from "../components/UserDetails"
 
 
 export default function AdminUsers() {
-  const {users, dispatch} = useUsersContext()
+  const { users, dispatch } = useUsersContext()
+  const [selectedYear, setSelectedYear] = useState('All');
 
   useEffect(() => {
     const fetchAllUsers = async () => {
@@ -15,32 +16,42 @@ export default function AdminUsers() {
       const json = await response.json()
 
       if (response.ok) {
-        dispatch({type: 'SET_USERS', payload: json})
+        dispatch({ type: 'SET_USERS', payload: json })
       }
     }
 
     fetchAllUsers();
   }, [])
 
-  useEffect(() => {
-    console.log("users updated", users)
-  }, [users])
+  const filteredUsers = selectedYear === 'All' ? users : users.filter(user => user.year === selectedYear)
 
   return (
-      <div className="flex flex-col w-[90%] m-auto my-8 gap-2">
-        <div className="flex flex-[0_0_75%] px-2">
-          <div className="flex-[1]">First Name</div>
-          <div className="flex-[1]">Last Name</div>
-          <div className="flex-[2]">Email</div>
-          <div className="flex-[1]">Mobile</div>
-          <div className="flex-[1]">Date of Birth</div>
-          <div className="flex-[1]">Gender</div>
-          {/*Spacer div for alignment*/}
-          <div className="flex-[0_0_21%]"></div>
-        </div>
-        {users && users.map((user: User) => (
-          <UserDetails key={user.id} user={user} />
-        ))}
+    <div className="flex flex-col w-[90%] m-auto my-8 gap-2">
+      <div className="flex gap-2">
+        <div className="">Filter by year:</div>
+        <select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)} className="">
+          <option value="All">All</option>
+          <option value="1st Year">1st Year</option>
+          <option value="2nd Year">2nd Year</option>
+          <option value="3rd Year">3rd Year</option>
+          <option value="4th Year">4th Year</option>
+          <option value="Post-graduate">Post-graduate</option>
+          <option value="Other">Other</option>
+        </select>
       </div>
+      <div className="flex flex-[0_0_75%] px-2">
+        <div className="flex-[1]">First Name</div>
+        <div className="flex-[1]">Last Name</div>
+        <div className="flex-[2]">Email</div>
+        <div className="flex-[1]">Mobile</div>
+        <div className="flex-[1]">Date of Birth</div>
+        <div className="flex-[1]">Gender</div>
+        {/*Spacer div for alignment*/}
+        <div className="flex-[0_0_21%]"></div>
+      </div>
+      {filteredUsers && filteredUsers.map((user: User) => (
+        <UserDetails key={user.id} user={user} />
+      ))}
+    </div>
   );
 }
