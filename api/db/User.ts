5ff77f625c0b10d56dd9ed6f809d0cc36fd9d930
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-07-18 14:49:16 +1200

    added yearOfStudy to user schema

--- api/db/User.ts
+++ api/db/User.ts
@@ -13,6 +13,10 @@ const userSchema = new Schema({
     maxLength: 40,
     minLength: 1,
   },
+  yearOfStudy: {
+    type: [String],
+    default: [],
+  },
 });
 
 const User = model('User', userSchema);
