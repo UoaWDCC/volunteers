2124f9fab7ad0d95dc60ecfd450b70e533dc3708
tugn205 <140466876+tugn205@users.noreply.github.com>
2024-07-21 16:19:58 +1200

    initial push, make changes in StaffAuth, add code in ProtectedRoute but with bugs

--- web/src/firebase/ProtectedRoute.tsx
+++ web/src/firebase/ProtectedRoute.tsx
@@ -1,4 +1,34 @@
-// Set up staff protected routes #129
-// https://github.com/orgs/UoaWDCC/projects/33?pane=issue&itemId=69456673
+import React, { useContext, ReactNode } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import AuthenticationContext from "../context/AuthenticationContext";
+import AccessLevel from "../enums/route.access.level";
 
+// Define the type for the props
+interface ProtectedRouteProps {
+  children: ReactNode;
+  accessLevelReq?: typeof AccessLevel[keyof typeof AccessLevel];
+}
 
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  children,
+  accessLevelReq = AccessLevel.USER,
+  ...rest
+}) => {
+  const { loading, user, VolUser } = useContext(AuthenticationContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <div className="loading loading-spinner" />;
+  }
+
+  if (user) {
+    if (VolUser.role === accessLevelReq || accessLevelReq === AccessLevel.USER) {
+      return <>{children}</>;
+    }
+    return <p>Access Denied!</p>;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} />;
+};
+
+export default ProtectedRoute;
