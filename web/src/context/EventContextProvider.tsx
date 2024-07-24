09c1fa9b0fe061948ab5d3de098e7d27fb31fa50
Taks07 <yutakaoishi7@gmail.com>
2024-07-24 13:12:02 +1200

    Added fetchEvents() function to EventContext

--- web/src/context/EventContextProvider.tsx
+++ web/src/context/EventContextProvider.tsx
@@ -7,19 +7,22 @@ export default function EventContextProvider({children}: { children: ReactNode }
     // Create a state variable to store events. Temporary value is an empty array
     const [events, setEvents] = useState([]);
 
-    // Get all events and set as default value of event state
-    fetch('http://localhost:3000/api/')
-    .then((res) => res.json())
-    .then((data) => {
-       console.log(data);
-       setEvents(data);
-    })
+    async function fetchEvents() {
+        // Fetch events from API
+        fetch("http://localhost:3000/api/")
+            .then(response => response.json())
+            .then(data => {
+                setEvents(data);
+            })
+            .catch(error => console.error(error));
+    }
 
     return (
         <EventContext.Provider
             value={{
                 events,
                 setEvents,
+                fetchEvents
             }}
         >
           {children}
