c8e3b2286533c42fa89010adad3e7cf1e24a4327
Taks07 <yutakaoishi7@gmail.com>
2024-07-25 19:52:32 +1200

    Created barebones LoginModal.tsx component

---
+++ web/src/components/LoginModal.tsx
@@ -0,0 +1,16 @@
+const LoginModal = () => {
+    return ( 
+        <dialog open>
+            <div>
+                <div>
+                    <h1>Log in or sign up</h1>
+                </div>
+                <div>
+                    <h2>Insert image here</h2>
+                </div>
+            </div>
+        </dialog>
+     );
+}
+ 
+export default LoginModal;
