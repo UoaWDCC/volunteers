adfea291789e4540f6808907e6bf73e044aade2f
Taks07 <yutakaoishi7@gmail.com>
2024-07-25 20:50:39 +1200

    Styled login modal to have a gray, fixed background above all other main page content

--- web/src/components/LoginModal.tsx
+++ web/src/components/LoginModal.tsx
@@ -2,14 +2,14 @@ import { useContext } from 'react';
 import LoginModalContext from '../context/LoginModalContext';
 
 const LoginModal = () => {
-    const { showModal } = useContext(LoginModalContext);
+    const { showModal, setShowModal } = useContext(LoginModalContext);
 
     if (!showModal) {
         return null;
     }
 
     return ( 
-        <div>
+        <div className='fixed z-[100] top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center' onClick={() => setShowModal(false)}>
             <div>
                 <div>
                     <h1>Log in or sign up</h1>
