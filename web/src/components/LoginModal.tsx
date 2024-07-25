cd19b8ed570fa21ae78db5f9d448a924449be168
Taks07 <yutakaoishi7@gmail.com>
2024-07-26 01:17:43 +1200

    Added google sign in button

--- web/src/components/LoginModal.tsx
+++ web/src/components/LoginModal.tsx
@@ -11,10 +11,16 @@ const LoginModal = () => {
     return ( 
         <div className='fixed z-[100] top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center' onClick={() => setShowModal(false)}>
             <div className='w-[940px] h-[560px] m-0 rounded-3xl bg-white flex justify-between items-center'>
+                <button className='absolute top-0 right-0 mt-[20px] mr-[20px]' onClick={() => setShowModal(false)} />
+                    <img src='/assets/loginModal/closeIcon.png' />
                 <div className='w-1/2 px-[55px] flex flex-col'> 
-                    <img src='/assets/club-logo.svg' className='self-center mb-[50px] mt-[-140px]' />
+                    <img src='/assets/club-logo.svg' className='self-center mb-[50px] mt-[-90px]' />
                     <h2 className='fontFamily-sans font-bold'>Log in or sign up</h2>
                     <p className='fontFamily-sans text-[14px]'>Track your hours, stay updated on events, and join our community of dedicated volunteers!</p>
+                    <button className='w-[345px] h-[45px] mt-[30px] flex justify-start items-center rounded-lg bg-transparent border-2 border-solid border-lightGrey'> 
+                        <img src='/assets/loginModal/googleIcon.png' className='w-[35px] h-[35px]' />
+                        <p className='fontFamily-sans font-semibold text-black text-[16px] ml-[40px] mt-[15px]'>Continue with Google</p>
+                    </button>
                 </div>
                 <div className='w-1/2'>
                     <img src='/assets/loginModal/sideImage.png' className='w-[470px] h-[560px] '/>
