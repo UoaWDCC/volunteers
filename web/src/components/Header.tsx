1be1e70b975de73bc002fad6e3468944a070a305
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2024-07-28 12:19:11 +1200

    pushing files for access for jerry to use

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,22 +1,56 @@
-import { handleGoogle } from "../firebase/firebase.tsx";
+import { handleGoogle } from '../firebase/firebase.tsx';
+// import AuthenticationContext from '../context/AuthenticationContext.tsx';
+import AuthenticationContext from '../context/AuthenticationContext.tsx';
+import { useContext } from 'react';
+import { getUserById } from '@utils/UserService.ts';
 
 function Header() {
+  // const { signInUsingGoogle } = useContext(AuthenticationContext) || { signInUsingGoogle: undefined };
+  // const authContext = useContext(AuthenticationContext) as AuthenticationContextProps;
+  // const { signInUsingGoogle } = authContext;
+  const authContext = useContext(AuthenticationContext);
+
+  if (!authContext) {
+    return null;
+  }
+
+  const { signInUsingGoogle, currentUser, signOut, userRole, getUserByStudentID } = authContext as { currentUser: { displayName: string }; signInUsingGoogle: () => void; signOut: () => void; userRole: string; setUserState: () => void; getUserByStudentID: (studentId: string) => void };
+  const isUserLoggedIn = currentUser !== null;
+
   return (
-    <header className="flex justify-center bg-neutral-tan">
-      <div className="flex flex-grow justify-between items-center min-w-min px-44 py-3">
+    <header className='flex justify-center bg-neutral-tan'>
+      <div className='flex flex-grow justify-between items-center min-w-min px-44 py-3'>
         <div>
-          <img loading="lazy" src="/assets/header-logo.svg" alt="Volunteers Club Logo" className="h-auto min-w-[150px]"/>
+          <img src='/public/assets/header-logo.svg' alt='Volunteers Club Logo' className='h-auto min-w-[150px]' />
         </div>
 
         <div>
-          <ul className="flex flex-row text-sm text-white font-mono m-0 gap-5">
-            <li onClick={ handleGoogle } className="px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer">Login</li>
-            <li className="px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer">Register</li>
+          <ul className='flex flex-row text-sm text-white font-mono m-0 gap-5'>
+            <li onClick={handleGoogle} className='px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer'>
+              Login
+            </li>
+            <li className='px-[30px] py-[10px] bg-primary rounded-[2rem] hover:underline cursor-pointer'>Register</li>
           </ul>
         </div>
       </div>
+      {!isUserLoggedIn && <button onClick={signInUsingGoogle}>Sign In</button>}
+      {isUserLoggedIn && <button onClick={signOut}>Sign Out</button>}
+      <button onClick={() => console.log(currentUser)}>log currentUser</button>
+      <div className='flex flex-col justify-center items-center'>
+        {isUserLoggedIn && <h3>current user: {currentUser.displayName} </h3>}
+        {isUserLoggedIn && <h3 className='inline'>user role: {userRole} </h3>}
+        {isUserLoggedIn && (
+          <h3 className='inline'>
+            test:{' '}
+            {(() => {
+              getUserByStudentID('1234567');
+              return 'User fetched';
+            })()}
+          </h3>
+        )}
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
--- web/src/components/MainGallery.tsx
+++ web/src/components/MainGallery.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import '../styles/componentStyles/MainGallery.css'
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 // define the props for the MainGallery component to accept
 interface MainGalleryProps {
-  data: { title: string; description: string; imgB: string;}[];
+  data: { title: string; description: string; imgB: string }[];
 }
 
 // data is an array of objects stored in json
@@ -29,27 +29,25 @@ const MainGallery = ({ data }: MainGalleryProps) => {
     setEventIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
   };
 
-  // // function to autoscroll the images, ?MIGHT REMOVE THIS LATER?
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setEventIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1))
-  //   }, 5000)
+  // function to autoscroll the images
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEventIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
+    }, 7000);
 
-  //   // return a cleanup function to clear the interval when the component unmounts
-  //   return () => clearInterval(interval)
-  // }, [data.length])
+    // return a cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [imgIndex, data.length]);
 
   return (
     <div className='gallery relative'>
-      <Link to={`/events/${imgIndex + 1}`}>
-        <div className='image flex overflow-hidden aspect-[8/3]'>
-          {data.map((event, index) => (
-            <img loading="lazy" src={event.imgB} key={index} className='picture object-cover w-[100%] h-[100%] shrink-0 grow-0 transition-translate 700ms ease-in-out duration-700' style={{ translate: `${-100 * imgIndex}%` }} />
-          ))}
-        </div>
-      </Link>
+      <div className='image flex overflow-hidden aspect-[8/3]'>
+        {data.map((event, index) => (
+          <img loading='lazy' src={event.imgB} key={index} className='picture object-cover w-[100%] h-[100%] shrink-0 grow-0 transition-translate 700ms ease-in-out duration-700' style={{ translate: `${-100 * imgIndex}%` }} />
+        ))}
+      </div>
 
-      <img loading="lazy" className='absolute z-20 right-0 bottom-[-0.07px] w-[100%]' src='..\public\assets\gallery\borders\GalleryBord.svg' />
+      <img loading='lazy' className='absolute z-20 right-0 bottom-[-1px] w-[100%]' src='..\public\assets\gallery\borders\GalleryBord.svg' draggable='false' />
 
       <div className='buttons z-50 absolute flex bg-[#ffffff00] text-white top-[47%] w-[100%]'>
         <button className='back ml-[2%] mr-auto scale-[1] h-[3.6em] rounded-full bg-[#e9e9e952] backdrop-blur-[4px] shadow-lg hover:bg-primary-dark hover:text-[#f7f7fb] active:bg-[#1a26449c] active:translate-y-1 transition-all ease-in-out duration-180' onClick={() => handleBack()}>
@@ -61,9 +59,11 @@ const MainGallery = ({ data }: MainGalleryProps) => {
       </div>
 
       <h1 className='title absolute top-[4%] left-[9%] z-20 text-[3.3vw] font-bold text-primary font-serif'>Make a Difference. Be a Volunteer.</h1>
-      <h1 className='title2 absolute bottom-[5.2%] left-[3%] z-20 text-white bg-[#e9e9e952] p-[1.3%] text-[2.4vw] rounded-3xl backdrop-blur-[4px] font-[600] shadow-lg '>{data[imgIndex].title}</h1>
+      <Link to={`/events/${imgIndex + 1}`}>
+        <h1 className='title2 absolute bottom-[5.2%] left-[3%] z-20 text-white bg-[#e9e9e952] p-[1.3%] text-[2.4vw] rounded-3xl backdrop-blur-[4px] font-[600] shadow-lg '>{data[imgIndex].title}</h1>
+      </Link>
       <button className='see-more absolute bottom-[2%] right-0 z-20 w-[33.1%] h-[13%] text-[1.3vw] rounded-3xl flex justify-center items-center gap-3 bg-primary font-mono hover:bg-primary-dark hover:text-[#f7f7fb] active:bg-[#264268] active:translate-y-1 transition-all ease-in-out duration-100' onClick={() => handleSeeMore}>
-        See other upcoming events! <FaArrowRight className='inline' size={20  } />
+        See other upcoming events! <FaArrowRight className='inline' size={20} />
       </button>
     </div>
   );
