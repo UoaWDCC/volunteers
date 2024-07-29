8fd191479782685a8c02c7f4b6e453c480329c7e
EzekielKo <eko034@aycklanduni.ac.nz>
2024-07-29 22:00:42 +1200

    admin edit events little touches

--- web/src/components/AdminEditEvent.tsx
+++ web/src/components/AdminEditEvent.tsx
@@ -44,44 +44,48 @@ const AdminEditEvent = ({
 
           <label htmlFor="eventTitle">Event Title: </label>
           <input
-            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-grey"
+            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-black"
             id="eventTitle"
             type="text"
             name="eventTitle"
             placeholder={title}
+            value={title}
             onChange={(e) => setTitle(e.target.value)}
             required
           />
 
           <label htmlFor="date">Date: </label>
           <input
-            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-grey"
+            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-black"
             id="date"
             type="date"
             name="date"
             placeholder={date}
+            value={date}
             required
             onChange={(e) => setDate(e.target.value)}
           />
 
           <label htmlFor="eventTime">Event Time: </label>
           <input
-            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-grey"
+            className="bg-lightGrey rounded-[10px] pl-[5px] pr-[5px] mr-[1em] text-black"
             id="eventTime"
             type="time"
             name="eventTime"
             placeholder={time}
+            value={time}
             required
             onChange={(e) => setTime(e.target.value)}
           />
 
           <label htmlFor="tag">Event Tag: </label>
           <input
-            className="bg-lightGrey rounded-[10px] pl-[1em] pr-[5px] mr-[1em] text-grey"
+            className="bg-lightGrey rounded-[10px] pl-[1em] pr-[5px] mr-[1em] text-black"
             id="tag"
             type="tag"
             name="tag"
             placeholder={tag}
+            value={tag}
             required
             onChange={(e) => setTag(e.target.value)}
           />
