5c39968f88ca16c7f92596eb93fc1b7119d99333
EzekielKo <eko034@aycklanduni.ac.nz>
2024-08-04 21:02:18 +1200

    :p

--- web/src/pages/Admin.tsx
+++ web/src/pages/Admin.tsx
@@ -4,9 +4,10 @@ export default function Admin() {
   return (
     <div className="flex flex-col justify-center my-4 text-2xl mt-[5em] font-semibold text-primary text-center">
       <div>
+        <h1>University of Auckland Volunteers Club</h1>
         <h1>Admin Page</h1>
       </div>
-      <div>
+      <div className="mt-[3em]">
         <Link to='/admin/users'>All users</Link>
         <Link className="ml-[5em]" to='/admin/events'>All events</Link>
       </div>
