5cc7f1c5fe7463dde9b7fa0555dea329290ce18d
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-08-06 15:08:58 +1200

    comment out ShowcaseUserAnnouncements

--- web/src/pages/MainMenu.tsx
+++ web/src/pages/MainMenu.tsx
@@ -9,14 +9,14 @@ import { eventHighlights } from '../data/EventHighlights.json';
 import OurAchievements from '@components/OurAchievements';
 import OurCommunity from '@components/OurCommunity';
 import AuthenticationContextProvider from '../context/AuthenticationContextProvider';
-import ShowCaseUserAnnouncements from '@components/ShowCaseUserAnnouncements';
+// import ShowCaseUserAnnouncements from '@components/ShowCaseUserAnnouncements';
 import LoginModalContextProvider from '../context/LoginModalContextProvider';
 import LoginModal from '@components/LoginModal';
 
 function MainMenu() {
   return (
     <div className="primary-background">
-      <ShowCaseUserAnnouncements/>
+      {/* <ShowCaseUserAnnouncements/> */}
       {/* <p className='font-serif text-font-primary text-lg px-h-md py-v-md'>font-serif for poppins,  font-lora for lora,  sans for Work Sans ::font-serif text-font-primary text-lg px-h-md py-v-md::</p> */}
       <AuthenticationContextProvider>
         <LoginModalContextProvider>
