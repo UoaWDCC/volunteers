32f865d1480288a3696d3874b42a7d45797ed636
Taks07 <yutakaoishi7@gmail.com>
2024-09-04 19:24:16 +1200

    Sponsor text is now black

--- web/src/components/Sponsor.tsx
+++ web/src/components/Sponsor.tsx
@@ -13,10 +13,10 @@ const Sponsor = ({logo, name, discount, website}: SponsorData) => {
 
     return (
         <Link to={website} className="flex flex-col items-center" onMouseEnter={() => {setShowDiscount(true)}} onMouseLeave={() => {setShowDiscount(false)}}>
-            <img loading="lazy" src={logo} alt={name} className={showDiscount ? "max-w-[100px] rounded-full" : "max-w-[100px] rounded-full grayscale"} />
+            <img src={logo} alt={name} className={showDiscount ? "max-w-[100px] rounded-full" : "max-w-[100px] rounded-full grayscale"} />
             {showDiscount && <div className="flex flex-col items-center z-10 absolute mt-[95px]">
                 <div className="w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[15px] border-b-lightGrey mt-2"></div>
-                <div className="bg-lightGrey h-[70px] rounded-[10px] flex flex-col justify-center">
+                <div className="text-black bg-lightGrey h-[70px] rounded-[10px] flex flex-col justify-center">
                     <div className="ml-[0.7rem] mt-[-0.7rem] text-[0.6rem]">{name}</div>
                     <div className="mx-auto px-[2.5rem] mt-[0.1rem]">{discount}</div>
                 </div>
