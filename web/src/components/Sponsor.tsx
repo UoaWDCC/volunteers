e423ef30cd901b926a5a0df84cc7d29d01430b63
Taks07 <yutakaoishi7@gmail.com>
2024-09-04 19:11:34 +1200

    Sponsor logo now only becomes coloured when moused over

--- web/src/components/Sponsor.tsx
+++ web/src/components/Sponsor.tsx
@@ -13,7 +13,7 @@ const Sponsor = ({logo, name, discount, website}: SponsorData) => {
 
     return (
         <Link to={website} className="flex flex-col items-center" onMouseEnter={() => {setShowDiscount(true)}} onMouseLeave={() => {setShowDiscount(false)}}>
-            <img loading="lazy" src={logo} alt={name} className="max-w-[100px] rounded-full" />
+            <img loading="lazy" src={logo} alt={name} className={showDiscount ? "max-w-[100px] rounded-full" : "max-w-[100px] rounded-full grayscale"} />
             {showDiscount && <div className="flex flex-col items-center z-10 absolute mt-[95px]">
                 <div className="w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[15px] border-b-lightGrey mt-2"></div>
                 <div className="bg-lightGrey h-[70px] rounded-[10px] flex flex-col justify-center">
