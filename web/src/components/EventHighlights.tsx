f67e05eca94d4c2bf716e794800908d2b037436e
hgao080 <hgao080@aucklanduni.ac.nz>
2024-09-06 18:22:36 +1200

    Adjusted event highlights to fit on screen

--- web/src/components/EventHighlights.tsx
+++ web/src/components/EventHighlights.tsx
@@ -58,24 +58,24 @@ const EventHighlights = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center relative bg-neutral overflow-hidden aspect-[1.41/1]">
+    <div className="flex flex-col justify-center items-center relative bg-neutral overflow-hidden h-screen">
       <div className="flex absolute w-full h-full overflow-hidden z-[2]">
         {data.map((event, index) => (
-          <div className="relative overflow-hidden flex-grow flex-shrink-0 basis-full transition-[translate] duration-[850ms] ease-in-out" key={index} style={{ translate: `${-100 * eventIndex}%` }}>
-            <div className="images">
-              <div className="absolute top-[5%] left-[13.5%] w-[21%]">
+          <div className="relative overflow-hidden flex-grow flex-shrink-0 basis-full transition- [translate] duration-[850ms] ease-in-out" key={index} style={{ translate: `${-100 * eventIndex}%` }}>
+            <div className="">
+              <div className="absolute top-[5%] left-[15%] w-[15%]">
                 <img className="object-cover w-full h-full rounded-sm" src={event.topLeftImage} />
               </div>
-              <div className="absolute bottom-[10%] left-[5%] w-[38%]">
+              <div className="absolute bottom-[10%] left-[10%] w-[32%]">
                 <img className="object-cover w-full h-full rounded-sm" src={event.bottomLeftImage} />
               </div>
-              <div className="absolute top-[19%] right-[22.5%] w-[11%]">
+              <div className="absolute top-[19%] right-[22.5%] w-[8%]">
                 <img className="object-cover w-full h-full rounded-sm" src={event.topRightImage} />
               </div>
-              <div className="absolute top-[40%] right-[0%] w-[27.5%]">
-                <img className="object-cover w-full h-full rounded-sm rounded-r-none" src={event.rightImage} />
+              <div className="absolute top-[40%] right-[0%] w-[22.5%]">
+                <img className="object-cover w-full h-full rounded-sm" src={event.rightImage} />
               </div>
-              <div className="absolute bottom-[2.5%] right-[30%] w-[12.5%]">
+              <div className="absolute bottom-[2.5%] right-[30%] w-[8%]">
                 <img className="object-cover w-full h-full rounded-sm" src={event.bottomRightImage} />
               </div>
             </div>
@@ -87,7 +87,7 @@ const EventHighlights = () => {
         {data.map((event, index) => (
           <li className="data-[active=true]:opacity-100 data-[active=true]:delay-0 opacity-0 flex absolute flex-col justify-start items-center transition-opacity duration-[850ms] ease-in-out delay-200" key={index} data-active={index == eventIndex ? "true" : null}>
             <h1 className="text-black text-center font-serif text-heading">{event.title}</h1>
-            <p className="text-black text-center font-serif text-detail">{event.description}</p>
+            <p className="text-black text-center font-serif text-detail w-[80%]">{event.description}</p>
           </li>
         ))}
       </ul>
@@ -101,9 +101,12 @@ const EventHighlights = () => {
         <IoIosArrowRoundForward className='absolute top-[45%] right-[6%] w-10 h-10 text-white bg-[#9FA1A6]/[0.9] rounded-full cursor-pointer hover:bg-black/[0.75] active:bg-[#464646]/[0.5] transform active:translate-y-[3px] shadow-lg transition-[all_ease-in-out_200ms] transition-[transform_ease-in-out_80ms]' onClick={() => handleForward()}/>
       </div>
 
-      <img loading="lazy" src="./assets/EventHighlights/swirl.svg" alt="" className="absolute w-[10%] h-auto bottom-[5%] left-[41%]"/>
-      <img loading="lazy" src="./assets/EventHighlights/flower.svg" alt="" className="absolute w-[8%] h-auto top-[30%] right-[18%]"/>
-      <img loading="lazy" src="./assets/EventHighlights/sparkle.svg" alt="" className="absolute w-[25%] h-auto top-[24%] left-0"/>
+      {/*Spiral*/}
+      <img loading="lazy" src="./assets/EventHighlights/swirl.svg" alt="" className="absolute w-[8%] h-auto bottom-[3%] left-[40.5%]"/>
+      {/*Flower*/}
+      <img loading="lazy" src="./assets/EventHighlights/flower.svg" alt="" className="absolute w-[7%] h-auto top-[29%] right-[18%]"/>
+      {/*Star*/}
+      <img loading="lazy" src="./assets/EventHighlights/sparkle.svg" alt="" className="absolute w-[20%] h-auto top-[22.5%] left-[4%]"/>
     </div>
   )
 }
