e52c9a379143545eedb15964bc0f1e7ec914ac84
hgao080 <hgao080@aucklanduni.ac.nz>
2024-09-07 11:52:59 +1200

    Added overlaying graphics for about us section

--- web/src/components/AboutUs.tsx
+++ web/src/components/AboutUs.tsx
@@ -1,6 +1,6 @@
 const AboutUs = () => {
   return (
-    <div className="flex flex-col items-center bg-primary pt-10 pb-36">
+    <div className="relative flex flex-col items-center bg-primary pt-10 pb-36">
       <div className="text-white bg-primary-light font-serif py-3 px-8 rounded-3xl text-section-header shadow">About Us</div>
       <div className="flex flex-col items-center justify-center bg-neutral-tan font-sans p-20 mt-16 rounded-3xl gap-4 shadow">
         <p className="m-0 text-3xl font-semibold">Welcome to the <img src="./assets/graphics/wheelDBlue.svg" className="inline"/> Uoa Volunteers Club,</p>
@@ -9,6 +9,13 @@ const AboutUs = () => {
         <p className="m-0 text-3xl font-semibold">Be a <span className="text-primary">catalyst</span> for change and create a</p>
         <p className="m-0 text-3xl font-semibold gap-1">brighter <img src="/assets/graphics/sparkleDRed.svg" className="inline" /> future with us!</p>
       </div>
+
+      {/* Top Left Graphic */}
+      <img src="/assets/graphics/highlightLRed.svg" className="absolute top-[-8%] left-[4%]" />
+      {/* Bottom Right Graphic */}
+      <img src="/assets/graphics/wheelLBlue.svg" className="absolute bottom-[-23%] right-[-5%] z-50" />
+      {/* Top Left Graphic */}
+      <img src="/assets/graphics/megaphone.svg" className="absolute bottom-[-15%] left-[0%]" />
     </div>
   );
 }
