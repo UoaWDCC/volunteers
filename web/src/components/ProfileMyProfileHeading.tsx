020979454a9806139a8a2f2e9608b3607506cb9f
Jos Badenas <jbad180@aucklanduni.ac.nz>
2024-09-08 14:05:02 +1200

    increased border raidus

--- web/src/components/ProfileMyProfileHeading.tsx
+++ web/src/components/ProfileMyProfileHeading.tsx
@@ -1,7 +1,7 @@
 
 const ProfileMyProfileHeading = ({ name }: { name: string }) => {
   return (
-    <div className="dashboard shadow-lg rounded-lg overflow-hidden w-[1035px] h-[256px] relative">
+    <div className="dashboard shadow-lg rounded-2xl overflow-hidden w-[1035px] h-[256px] relative">
       {/* Blue header section */}
       <div className="dashboard bg-primary h-[160px]"></div>
 
@@ -11,7 +11,7 @@ const ProfileMyProfileHeading = ({ name }: { name: string }) => {
         <span className="text-heading1 font-bold text-black ml-[150px]">{name}</span>
       </div>
 
-      {/* Profile image placeholder (overlaying both blue and white sections) */}
+      {/* Profile image placeholder */}
       <div className="absolute top-[96px] left-6">
         <div className="bg-black w-[129px] h-[129px] rounded-full"></div>
       </div>
