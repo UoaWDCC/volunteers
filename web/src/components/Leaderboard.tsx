0d15574555266567a13aae6ea8b289af118a7d73
Taks07 <yutakaoishi7@gmail.com>
2024-09-10 13:27:58 +1200

    Set up dashboard leaderboard

---
+++ web/src/components/Leaderboard.tsx
@@ -0,0 +1,9 @@
+function Leaderboard() {
+    return (
+        <div className="h-full w-4/5 pt-7 flex flex-col items-center bg-white rounded-2xl">
+        <h2 className="text-primary text-section-header font-semibold">Leaderboard</h2>
+        </div>
+    );
+}
+
+export default Leaderboard;
