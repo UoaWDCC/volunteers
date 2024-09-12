1dcfe4d92d216b62f381981cdbfa31da0f7631e4
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-09-12 13:48:16 +1200

    position of welcome stats elements

--- web/src/components/WelcomeStats.tsx
+++ web/src/components/WelcomeStats.tsx
@@ -16,26 +16,28 @@ function WelcomeStats() {
           Welcome back {User}!
         </h1>
         <p className="text-gray-500">Glad to see you're back in action!</p>
-        <div className="mt-4">
-          <div className="flex justify-between text-sm font-semibold text-gray-500">
-            <span>Level 2</span>
-            <span>Level 3</span>
-          </div>
-          <div className="bg-gray-300 rounded-full h-2 mt-2 relative">
-            <div className="bg-blue-500 h-2 rounded-full w-[70%]"></div>
-          </div>
-          <p className="text-sm text-gray-500 mt-2">6 more hours to go!</p>
-        </div>
-      </div>
 
-      <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold">{Hours}</span>
-        <span className="text-gray-500">Hours</span>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="mt-4 w-[100%]">
+            <div className="flex justify-between text-sm font-semibold text-gray-500">
+              <span>Level 2</span>
+              <span>Level 3</span>
+            </div>
+            <div className="bg-gray-300 rounded-full h-3 mt-2 relative">
+              <div className="bg-blue-500 h-3 rounded-full w-[70%]" />
+            </div>
+            <p className="text-sm text-gray-500 mt-2">6 more hours to go!</p>
+          </div>
+          <div className="mt-6">
+            <span className="text-4xl font-bold">{Hours}</span>
+            <span className="text-gray-500">Hours</span>
+          </div>
+        </div>
       </div>
 
       <div className="ml-6">
         <img
-          src="https://via.placeholder.com/100"
+          src="./public/assets/dashboard/welcomeHandHeart.png"
           alt="Heart Hands"
           className="w-20 h-20"
         />
