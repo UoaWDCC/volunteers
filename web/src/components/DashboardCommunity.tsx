07f9338e373ec8346c5a55a4f7288a744f91e228
Taks07 <yutakaoishi7@gmail.com>
2024-09-12 12:42:49 +1200

    Changed around size of leaderboard and community dashboard divs

--- web/src/components/DashboardCommunity.tsx
+++ web/src/components/DashboardCommunity.tsx
@@ -5,15 +5,11 @@ function DashboardCommunity() {
   return (
     <div className="overflow-hidden flex flex-row w-[100%] px-5 gap-5">
 
-
-                {/* width of the gallery */}
-                <div className='flex flex-col w-[70%]'>
+                <div className='flex flex-col w-[80%]'>
                     <CommunityGalleryWhole />
                 </div>
                     
-                {/* width of the leadboard thing thing */}
-                <div className='bg-green-100 w-[30%] h-[40vw]'>
-                    {/* place thing component here and remove bg-green */}
+                <div className='flex justify-end w-[20%] h-[40vw]'>
                     <Leaderboard />
                 </div>
             
