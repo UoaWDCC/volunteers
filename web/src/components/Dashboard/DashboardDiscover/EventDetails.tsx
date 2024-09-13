48d2c570d2a7d531b23c6088c47634b894a3b095
Taks07 <yutakaoishi7@gmail.com>
2024-09-13 15:37:09 +1200

    Added image

--- web/src/components/Dashboard/DashboardDiscover/EventDetails.tsx
+++ web/src/components/Dashboard/DashboardDiscover/EventDetails.tsx
@@ -21,25 +21,8 @@ export default function EventDetails({event}: EventProps) {
     const endDate = new Date(event.end_date_time);
 
     return (
-        <div className="fixed left-[15%] top-[10%] flex flex-col w-[85%] h-[90%] px-5 bg-[#F7F7FB]"> {/* event-container */}
-            <div className="w-3/4 flex items-center">
-                <div className="inline m-5"> {/* image-container */}
-                    <img loading="lazy" className="h-24 w-40 object-cover rounded-md" src={event.image} />
-                </div>
-                
-                <div className="inline w-1/2  p-2"> {/* event details on the right */}
-                    <p className="mb-0 text-sm">{startDate.toISOString()}</p>
-                    <h2 className="inline font-semibold text-base mb-1 text-xl">{event.event_title}</h2>
-                    <p className="mb-0 text-sm">{event.location}</p>
-                </div>
-            </div>
-            <div className="ml-auto p-6">
-                <p className="text-right block text-xs font-semibold">{event.host}</p>
-                <div className="">  
-                    <p className="text-right block text-xs m-0">30 interested</p>
-                    <p className="text-right block text-xs">15 going</p>
-                </div>
-            </div>
+        <div className="flex flex-col items-center fixed left-[15%] top-[10%] w-[85%] h-[90%] px-5 pt-5 bg-[#F7F7FB] overflow-scroll scrollbar-none"> {/* event-container */}
+            <img src={event.image} className=" w-[95%] h-[45vh] rounded-3xl" />
         </div>
     )
 }
