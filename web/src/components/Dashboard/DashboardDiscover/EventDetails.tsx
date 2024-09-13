cde5762125cc8dfb62bee7993b3d30e182354387
Taks07 <yutakaoishi7@gmail.com>
2024-09-13 15:59:46 +1200

    Added title, tags and register button

--- web/src/components/Dashboard/DashboardDiscover/EventDetails.tsx
+++ web/src/components/Dashboard/DashboardDiscover/EventDetails.tsx
@@ -23,6 +23,20 @@ export default function EventDetails({event}: EventProps) {
     return (
         <div className="flex flex-col items-center fixed left-[15%] top-[10%] w-[85%] h-[90%] px-5 pt-5 bg-[#F7F7FB] overflow-scroll scrollbar-none"> {/* event-container */}
             <img src={event.image} className=" w-[95%] h-[45vh] rounded-3xl" />
+
+            <div className="flex flex-col w-full px-10 py-4">
+                <div className="flex flex-row justify-between items-center w-full">
+                    <h1 className="text-subheading font-bold">{event.event_title}</h1>
+                    <button className="h-10 text-body-heading rounded-full">Register for Event</button>
+                </div>
+
+                <div className="flex flex-row justify-start gap-3 w-full">
+                    {event.tag.map((tag, index) => (
+                        <div key={index} className="text-xs pt-[3px] px-3 h-6 text-grey border-grey border-solid border-2 rounded-full ">{tag}</div>
+                    ))}
+                </div>
+
+            </div>
         </div>
     )
 }
