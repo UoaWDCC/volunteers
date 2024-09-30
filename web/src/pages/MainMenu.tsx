214696983a5c70f2f58d8b8e9c3eb13111c15e39
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-09-30 23:47:40 +1300

    move unused imports

--- web/src/pages/MainMenu.tsx
+++ web/src/pages/MainMenu.tsx
@@ -1,31 +1,30 @@
-import Header from '@components/main/Header';
-import Footer from '../components/main/Footer';
-import '../styles/pageStyles/MainMenu.css';
-import MainGallery from '@components/main/MainGallery';
-import SponsorsList from '@components/main/SponsorsList';
-import EventHighlights from '@components/main/EventHighlights';
-import OurAchievements from '@components/main/OurAchievements';
-import OurCommunity from '@components/main/OurCommunity';
-import AuthenticationContextProvider from '../context/AuthenticationContextProvider';
+import Header from "@components/main/Header";
+import Footer from "../components/main/Footer";
+import "../styles/pageStyles/MainMenu.css";
+import MainGallery from "@components/main/MainGallery";
+import SponsorsList from "@components/main/SponsorsList";
+import EventHighlights from "@components/main/EventHighlights";
+import OurAchievements from "@components/main/OurAchievements";
+import OurCommunity from "@components/main/OurCommunity";
 import LoginModalContextProvider from "../context/LoginModalContextProvider";
 import LoginModal from "@components/main/LoginModal";
-import AboutUs from '@components/main/AboutUs';
+import AboutUs from "@components/main/AboutUs";
 
 function MainMenu() {
   return (
     <div className="overflow-hidden">
       {/* <p className='font-serif text-font-primary text-lg px-h-md py-v-md'>font-serif for poppins,  font-lora for lora,  sans for Work Sans ::font-serif text-font-primary text-lg px-h-md py-v-md::</p> */}
-        <LoginModalContextProvider>
-          <LoginModal />
-          <Header />
-          <MainGallery />
-          <AboutUs />
-          <EventHighlights />
-          <OurAchievements />
-          <OurCommunity />
-          <SponsorsList />
-          <Footer />
-        </LoginModalContextProvider>
+      <LoginModalContextProvider>
+        <LoginModal />
+        <Header />
+        <MainGallery />
+        <AboutUs />
+        <EventHighlights />
+        <OurAchievements />
+        <OurCommunity />
+        <SponsorsList />
+        <Footer />
+      </LoginModalContextProvider>
     </div>
   );
 }
