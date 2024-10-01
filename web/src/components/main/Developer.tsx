0d1748bd8d112ec7c6c17a0e4b651b0103ae1075
EzekielKo <eko034@aycklanduni.ac.nz>
2024-10-01 22:59:29 +1300

    styled team page

--- web/src/components/main/Developer.tsx
+++ web/src/components/main/Developer.tsx
@@ -2,10 +2,11 @@ import linkedinLogo from "../../../public/assets/linkedin_logo.png"
 const Developer = ({firstName, lastName, major, profilePic, role, yearLevel, linkedin}:any) => {
     return (
         <div className="
-        flex flex-col pt-[1em]
-        items-center text-center
-        border-2 rounded-[5%] border-primary-light hover:border-primary-dark
-        m-[0.5em] p-0 w-[15em] h-[19em] font-mono">
+            z-10
+            flex flex-col pt-[1em]
+            items-center text-center
+            border-2 rounded-[5%] border-primary-light hover:border-primary-dark
+            m-[0.5em] p-0 w-[15em] h-[19em] font-mono">
             <h6 className="break-words font-bold text-[1.3em]">{role}</h6>
             <img className="object-cover w-[5em] h-[5em] rounded-full mb-2 mt-[5px]" src={profilePic} alt="Profile Picture"/>
             <h6 className="break-words text-primary text-[19px] font-bold">{firstName} {lastName}</h6>
