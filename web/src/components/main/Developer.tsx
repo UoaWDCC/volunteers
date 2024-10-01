673bbf8c902f06e8e650415c1bcc4e483d91bd1c
EzekielKo <eko034@aycklanduni.ac.nz>
2024-10-01 22:25:55 +1300

    created credits page

--- web/src/components/main/Developer.tsx
+++ web/src/components/main/Developer.tsx
@@ -1,12 +1,21 @@
-const Developer = ({firstName, lastName, major, profilePic, role, yearLevel}:any) => {
+import linkedinLogo from "../../../public/assets/linkedin_logo.png"
+const Developer = ({firstName, lastName, major, profilePic, role, yearLevel, linkedin}:any) => {
     return (
-        <div>
-            <h6>{firstName}</h6>
-            <h6>{lastName}</h6>
-            <h6>{major}</h6>
-            <h6>{profilePic}</h6>
-            <h6>{role}</h6>
-            <h6>{yearLevel}</h6>
+        <div className="
+        flex flex-col pt-[1em]
+        items-center text-center
+        border-2 rounded-[5%] border-primary-light hover:border-primary-dark
+        m-[0.5em] p-0 w-[15em] h-[19em] font-mono">
+            <h6 className="break-words font-bold text-[1.3em]">{role}</h6>
+            <img className="object-cover w-[5em] h-[5em] rounded-full mb-2 mt-[5px]" src={profilePic} alt="Profile Picture"/>
+            <h6 className="break-words text-primary text-[19px] font-bold">{firstName} {lastName}</h6>
+            <h6 className="break-words">{yearLevel}</h6>
+            <h6 className="break-words p-[5px] leading-[1.2em]">{major}</h6>
+            <div className="mt-auto mt-[0px]">
+                <a href={linkedin} target="_blank" className="">
+                    <img className="w-[2em]" src={linkedinLogo} alt="LinkedIn Logo"/>
+                </a>
+            </div>
         </div>
     )
 }
--- web/src/components/main/Developers.tsx
+++ web/src/components/main/Developers.tsx
@@ -5,7 +5,8 @@ import Developer from "./Developer";
 
 const Developers = () => {
     const colref = collection(db, "developers-first-gen");
-    const [usersFirst, setUsersFirst] = useState<any[]>([]);
+    const [leads, setLeads] = useState<any[]>([]);
+    const [devs, setDevs] = useState<any[]>([]);
 
     useEffect(() => {
         getDocs(colref)
@@ -14,8 +15,23 @@ const Developers = () => {
             snapshot.docs.forEach((doc) => {
                 getUsersFirst.push({ ...doc.data(), id: doc.id });
             });
-            console.log(getUsersFirst);
-            setUsersFirst(getUsersFirst);
+            getUsersFirst.sort((a, b) => {
+                if (a.last_name < b.last_name) return -1;
+                if (a.last_name > b.last_name) return 1;
+                return 0;
+            });
+            setLeads(getUsersFirst.filter(user => user.role === 'Project Manager'))
+            const combinedLeads = [
+                ...getUsersFirst.filter(user => user.role === 'Project Manager'),
+                ...getUsersFirst.filter(user => user.role === 'Tech Lead'),
+                ...getUsersFirst.filter(user => user.role === 'Designer')
+            ];
+            setDevs(getUsersFirst.filter(user => 
+                user.role !== 'Project Manager' &&
+                user.role !== 'Tech Lead' &&
+                user.role !== 'Designer'
+            ));
+            setLeads(combinedLeads)
         })
         .catch((err) => {
             console.log(err.message);
@@ -24,17 +40,29 @@ const Developers = () => {
 
     return (
         <div>
-            <div className="font-bold text-primary font-serif text-[3.3vw] text-center mt-[1em]">Meet the Team!</div>
-            <h1 className="font-bold text-primary-dark font-serif text-[2vw] text-center mt-[1em]">First Gen: 2024</h1>
-            <div className="flex items-center">
-                {usersFirst.map((developer:any) => (
+            <div className="font-bold text-primary font-serif text-[3em] text-center mt-[1em]">Meet the Team!</div>
+            <h1 className="font-bold text-primary-dark font-serif text-[2em] text-center mt-[1em]">First Gen: 2024</h1>
+            <div className="flex justify-center text-[1em] flex flex-wrap ml-[5em] mr-[5em] mb-[5em] max-w-[100vw] flex-wrap">
+                    {leads.map((developer:any) => (
                     <Developer 
                         firstName={developer.first_name}
                         lastName={developer.last_name}
                         major={developer.major}
-                        profilePic={developer.profile_picture}
+                        profilePic={developer.profile_pic}
                         role={developer.role}
                         yearLevel={developer.year_level}
+                        linkedin={developer.linkedin}
+                    />
+                ))}
+                {devs.map((developer:any) => (
+                    <Developer 
+                        firstName={developer.first_name}
+                        lastName={developer.last_name}
+                        major={developer.major}
+                        profilePic={developer.profile_pic}
+                        role={developer.role}
+                        yearLevel={developer.year_level}
+                        linkedin={developer.linkedin}
                     />
                 ))}
             </div>
