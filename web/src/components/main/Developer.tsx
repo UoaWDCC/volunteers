43f61edfb94b28dd030a7dd730fa9616ce3b2a55
EzekielKo <eko034@aycklanduni.ac.nz>
2024-10-04 11:57:46 +1300

    changed some coloours

--- web/src/components/main/Developer.tsx
+++ web/src/components/main/Developer.tsx
@@ -9,21 +9,21 @@ const Developer = ({firstName, lastName, major, profilePic, role, yearLevel, lin
             m-[0.5em] p-0 w-[15em] h-[19em] font-mono"
             >
             <div className="
-                border-4 rounded-[10px] px-[0.5em] border-white
+                border-4 rounded-[10px] px-[0.5em] border-neutral
                 bg-primary
             ">
                 <h6 className="font-bold text-[1.3em] text-white">{role}</h6>
             </div>
             <img className="
                 object-cover w-[5em] h-[5em] rounded-full mb-2 mt-[5px]
-                border-4 border-white
+                border-4 border-neutral
             " src={profilePic} alt="Profile Picture"/>
             <h6 className="break-words text-primary text-[19px] font-bold">{firstName} {lastName}</h6>
             <h6 className="break-words">{yearLevel}</h6>
             <h6 className="break-words p-[5px] leading-[1.2em]">{major}</h6>
             <div className="mt-auto pb-[1em]">
                 <a href={linkedin} target="_blank" className="relative flex">
-                    <img className="w-[2.3em] border-2 border-white rounded-[3px]" src={linkedinLogo} alt="LinkedIn Logo"/>
+                    <img className="w-[2.3em] border-2 border-neutral rounded-[3px]" src={linkedinLogo} alt="LinkedIn Logo"/>
                     <span className="absolute text-[0.8em] bottom-[-10px] right-[-8px]">&trade;</span>
                 </a>
             </div>
