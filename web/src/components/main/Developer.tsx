0a74e9cd49ef5382da17ff325e0540c670f9a194
EzekielKo <eko034@aycklanduni.ac.nz>
2024-10-07 19:50:38 +1300

    clicking anywhere on the div will redirect

--- web/src/components/main/Developer.tsx
+++ web/src/components/main/Developer.tsx
@@ -7,6 +7,7 @@ const Developer = ({firstName, lastName, major, profilePic, role, yearLevel, lin
             items-center text-center
             border-2 rounded-[15px] border-primary-light hover:border-primary-dark hover:shadow-lg hover:shadow-primary-dark hover:bg-neutral
             m-[0.5em] p-0 w-[15em] h-[19em] font-mono"
+            onClick={() => window.open(linkedin, "_blank")}
             >
             <div className="
                 border-4 rounded-[10px] px-[0.5em] border-neutral
@@ -21,6 +22,7 @@ const Developer = ({firstName, lastName, major, profilePic, role, yearLevel, lin
             <h6 className="break-words text-primary text-[19px] font-bold">{firstName} {lastName}</h6>
             <h6 className="break-words">{yearLevel}</h6>
             <h6 className="break-words p-[5px] leading-[1.2em]">{major}</h6>
+            
             <div className="mt-auto pb-[1em]">
                 <a href={linkedin} target="_blank" className="relative flex">
                     <img className="w-[2.3em] border-2 border-neutral rounded-[3px]" src={linkedinLogo} alt="LinkedIn Logo"/>
