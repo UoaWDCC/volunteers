af0d54c1cc289098594c3869996a33bd4425dab6
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2024-10-09 20:32:17 +1300

    Update CommunityGalleryWhole.tsx

--- web/src/components/Dashboard/DashboardCommunity/CommunityGalleryWhole.tsx
+++ web/src/components/Dashboard/DashboardCommunity/CommunityGalleryWhole.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+//import axios from "axios";
+//import { useState, useEffect } from "react";
 
 import CommunityGallery from "./CommunityGallery";
 
