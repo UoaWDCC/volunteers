96e46027d0db706054bcd2d1abd78c0aec1e48bd
Joey Back <jbac208@aucklanduni.ac.nz>
2025-03-17 12:05:10 +1300

    setup community project

---
+++ fireCMS/my-cms/src/firebase_config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    "projectId": "volunteerclub-14b7a",
+    "appId": "1:765497707892:web:4e3d502ed883357b037021",
+    "storageBucket": "volunteerclub-14b7a.appspot.com",
+    "locationId": "australia-southeast1",
+    "apiKey": "AIzaSyBox7z0b2SBEnz0v6qmQjFx3QfUJcG099U",
+    "authDomain": "volunteerclub-14b7a.firebaseapp.com",
+    "messagingSenderId": "765497707892",
+    "measurementId": "G-0N8KPK7M6X"
+}
