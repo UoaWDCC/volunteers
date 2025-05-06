6517f036c61c2fea010cc1a4ed5d69d5f9a68cfe
jakonito10 <jankarlorn@gmail.com>
2025-05-07 00:56:24 +1200

    Create skeleton for component structure

--- web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
+++ web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
@@ -1,7 +1,17 @@
+import Notifications from "./Notifications";
+
 const DashboardAdmin: React.FC = () => {
     return (
         <div className="flex flex-row w-full h-full gap-5">
-            Test
+            {/* Volunteer Events section */}
+            <div className="w-[75%]">
+
+            </div>
+
+            {/* Notifications and Newsletter section */}
+            <div className="w-[20%]">
+                <Notifications />
+            </div>
         </div>
     );
 }
