568b3696c63b416f7aeaf548ed7468e68f30954b
jakonito10 <jankarlorn@gmail.com>
2025-05-07 00:55:34 +1200

    Add temp. conditional rendering logic for admin dashboard

---
+++ web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
@@ -0,0 +1,9 @@
+const DashboardAdmin: React.FC = () => {
+    return (
+        <div className="flex flex-row w-full h-full gap-5">
+            Test
+        </div>
+    );
+}
+
+export default DashboardAdmin;
