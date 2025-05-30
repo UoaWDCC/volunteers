ba04f032e9f4ec36e128c27d6d1b51bad147572d
pche314 <Pche314@aucklanduni.ac.nz>
2025-05-31 01:44:35 +1200

    Add/➕: index and tail.wind.config
    Index and tailwind.config were altered a little to have things better suited for me.
    
    I also created the filter and search buttons in the admin board.

--- web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
+++ web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
@@ -3,14 +3,14 @@ import VolunteerEvents from "./VolunteerEvents";
 
 const DashboardAdmin: React.FC = () => {
     return (
-        <div className="flex flex-row w-full h-full gap-5">
+        <div className="flex flex-row w-full h-[88vh] gap-8">
             {/* Volunteer Events section */}
-            <div className="w-[75%]">
+            <div className="w-[70%] h-full gap-8">
                 <VolunteerEvents />
             </div>
 
             {/* Notifications and Newsletter section */}
-            <div className="flex flex-col w-[20%] gap-8">
+            <div className="flex flex-col w-[25%] h-full gap-8">
                 <Notifications />
             </div>
         </div>
