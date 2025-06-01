9e25ac2099944c732827c973f5168e371d56e3ec
pche314 <Pche314@aucklanduni.ac.nz>
2025-06-01 23:48:22 +1200

    #344 Cleaning up community
    Deleting useless wrapper for DashBoardComminityGallery

--- web/src/components/Dashboard/DashboardCommunity/DashboardCommunity.tsx
+++ web/src/components/Dashboard/DashboardCommunity/DashboardCommunity.tsx
@@ -1,11 +1,11 @@
-import CommunityGalleryWhole from "../DashboardCommunity/CommunityGalleryWhole";
+import CommunityGallery from "../DashboardCommunity/CommunityGallery";
 import Leaderboard from "../Leaderboard";
 
 function DashboardCommunity() {
   return (
     <div className="flex flex-row w-full h-full gap-5">
       <div className="flex flex-col w-[75%] h-full overflow-y-auto scrollbar-none shadow-lg rounded-lg">
-        <CommunityGalleryWhole />
+        <CommunityGallery/>
       </div>
 
       <div className="w-[20%]">
