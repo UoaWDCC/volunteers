a972671534e74b65cd707935b5fa179f9bd8bfce
pche314 <Pche314@aucklanduni.ac.nz>
2025-06-01 23:18:29 +1200

    Sty/🎨: Dashboard and newsletter margins

--- web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
+++ web/src/components/Dashboard/DashboardAdmin/DashboardAdmin.tsx
@@ -4,16 +4,20 @@ import VolunteerEvents from "./VolunteerEvents";
 
 const DashboardAdmin: React.FC = () => {
     return (
-        <div className="flex flex-row w-full h-[88vh] gap-10 p-8">
+        <div className="flex flex-row w-full h-[88vh] gap-6 p-6">
             {/* Volunteer Events section */}
-            <div className="w-[70%] h-full gap-8 ">
+            <div className="w-[70%] h-full gap-6 ">
                 <VolunteerEvents />
             </div>
 
             {/* Notifications and Newsletter section */}
-            <div className="flex flex-col w-[22%] gap-8">
-                <Announcements />
-                <Newsletters />
+            <div className="flex flex-col w-[28%] h-full gap-6">
+                <div className="flex flex-col flex-1">
+                    <Announcements />
+                </div>
+                <div className="flex flex-col flex-1">
+                    <Newsletters />
+                </div>
             </div>
         </div>
     );
--- web/src/components/Dashboard/DashboardAdmin/Newsletters.tsx
+++ web/src/components/Dashboard/DashboardAdmin/Newsletters.tsx
@@ -43,20 +43,14 @@ const Newsletters: React.FC = () => {
             <hr className="border-t-2 border-gray-300 rounded-full my-2" />
 
             {/* Notification cards*/}
-            <div className="flex flex-col gap-6 h-[25vh] overflow-y-scroll">
+            <div className="flex flex-col overflow-hidden overflow-y-scroll gap-6">
                 {newsletters.map((newsletter) => (
                     <NewsletterCard newsletter={newsletter} key={newsletter.id} />
                 ))}
             </div>
 
             {/* Create button */}
-            <div className="flex bg-white mt-3 justify-center">
-                <button className="bg-primary text-body text-xs rounded-full py-3 px-5 hover:bg-primary-dark hover:text-[#f7f7fb] active:bg-[#264268] active:translate-y-0.5 transition-all ease-in-out duration-100 max-[1440px]:text-lg max-[1440px]:py-1 max-[1440px]:px-7 max-[1280px]:py-2 max-[1280px]:px-9 max-[1280px]:text-xl">
-                    <div className="flex items-center gap-2">
-                        <p className="m-0 max-[1440px]:text-[14px] max-[1280px]:text-[1rem]" onClick={ handleCreateNewsletter }>Create</p>
-                    </div>
-                </button>
-            </div>
+           <button className="rounded-full w-[40%] self-center"onClick={ handleCreateNewsletter }>Create</button>
         </div>
     )
 };
