4be37a3e99b5b2449952bccfa69637452e6e272d
pche314 <Pche314@aucklanduni.ac.nz>
2025-06-02 00:02:15 +1200

    Fix/🔧:Spacing and overflow fix #344

--- web/src/components/Dashboard/DashboardCommunity/CommunityGallery.tsx
+++ web/src/components/Dashboard/DashboardCommunity/CommunityGallery.tsx
@@ -55,8 +55,8 @@ const CommunityGallery = () => {
 
   return (  
       <div className='bg-white rounded-3xl py-10 px-[4%] w-full text-subheading text-black shadow-lg'>
-          <p>People you may know: </p>
-          <div className='w-full grid grid-cols-3 max-[1280px]:grid-cols-2 min-[2100px]:grid-cols-4 min-[2560px]:grid-cols-5 gap-[2%]'>
+          <p>Our Community: </p>
+          <div className='w-full grid grid-cols-3 gap-x-[2%] gap-y-4 max-[1280px]:grid-cols-2 min-[2100px]:grid-cols-4 min-[2560px]:grid-cols-5 overflow-y-auto scrollbar-none'>
             {filteredData.map((user: userData, index: number) => (
               <CommunityGalleryCard key={index} user={user} />
             ))}
