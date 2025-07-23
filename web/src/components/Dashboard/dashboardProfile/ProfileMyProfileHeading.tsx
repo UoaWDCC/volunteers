f819de02bef62e6a506065ce440bd0a5e0cb23b5
Vikram Varadarajan <vvar983@aucklanduni.ac.nz>
2025-07-23 13:29:44 +1200

    Feat/updating profile pic (#368)
    * Added handleImageChange function in ProfileEditModal
    
    * #333 added comment on function handleImageChange
    
    * #333 Added try and catch on handleImageChange, currently profile image fails every time but on click error message outputted correctly
    
    * #333 Added to signup page to add default profile image
    
    * #333 Added to ProfileMyProfileHeading to display user profile picture
    
    * #333 added new route to upload profile picture to backend
    
    * #333 Installed multer, attempting to correct file upload
    
    * #333 Added openFileDialog to allow user to access device files
    
    * #333 Added multer to package.json
    
    * #333 Added uploadProfilePicture async function to userController and edited path in users.ts to use a new function
    
    * #333 Added profile image to appear on dashboard header as well
    
    * #333 Added profile_picture field to update requirement to prevent default updating
    
    * added 2MB pfp file size limit
    
    ---------
    
    Co-authored-by: Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
    Co-authored-by: Joey Back <jbac208@aucklanduni.ac.nz>
    Co-authored-by: Joey Back <117869870+jbac208@users.noreply.github.com>

--- web/src/components/Dashboard/dashboardProfile/ProfileMyProfileHeading.tsx
+++ web/src/components/Dashboard/dashboardProfile/ProfileMyProfileHeading.tsx
@@ -1,5 +1,10 @@
+import { useContext } from "react";
+import AuthenticationContext from "../../../context/AuthenticationContext";
 
 const ProfileMyProfileHeading = ({ name }: { name: string }) => {
+  const authContext = useContext(AuthenticationContext);
+  const firestoreUserDetails = authContext?.firestoreUserDetails;
+
   return (
     <div className="dashboard shadow-lg rounded-xl overflow-hidden w-full h-full flex flex-col relative">
       {/* Blue header section */}
@@ -13,7 +18,13 @@ const ProfileMyProfileHeading = ({ name }: { name: string }) => {
 
       {/* Profile image placeholder */}
       <div className="absolute bottom-[15%] left-10">
-        <div className="bg-black w-[8rem] h-[8rem] rounded-full"></div>
+        <div className="bg-black w-[8rem] h-[8rem] rounded-full">
+          <img
+            src={firestoreUserDetails?.profile_picture || '/assets/profile-placeholder.png'}
+            alt="Profile Placeholder"
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
