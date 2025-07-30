d63896b42a1d3f505a44ebf03d2794b9e32f14a8
Igordro1d <igormoreiracastro0@gmail.com>
2025-07-30 22:44:57 +1200

    Add GET and POST routes for fetching and saving newsletter titles

--- api/routes/api/newsletter.ts
+++ api/routes/api/newsletter.ts
@@ -1,9 +1,13 @@
 const express = require("express");
-import { createNewsletter } from '../../controllers/newsletterController';
+import { createNewsletter, getAllNewsletters, saveNewsletterTitle } from '../../controllers/newsletterController';
 
 const router = express.Router();
 
 // POST route to create and format a newsletter
 router.post('/create', createNewsletter);
+
+router.get('/titles', getAllNewsletters);
+
+router.post('/titles', saveNewsletterTitle);
 
 export default router;
