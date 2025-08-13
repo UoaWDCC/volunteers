fabe58e5cb577dbf15e988edba6f9583bcc77056
pche314 <Pche314@aucklanduni.ac.nz>
2025-08-13 12:09:08 +1200

    add: implemented the cms button #371
    - button added
    - added authContext to the header
    - added admin handling logic to the header

--- web/src/components/Dashboard/DashboardHeader.tsx
+++ web/src/components/Dashboard/DashboardHeader.tsx
@@ -1,13 +1,17 @@
 import { IoIosNotifications } from "react-icons/io";
 import NotificationTab from "@components/Dashboard/NotificationTab";
 import SearchBar from "@components/Dashboard/DashboardCommunity/SearchBar";
-import { useState } from "react";
+import AuthenticationContext from "../../context/AuthenticationContext.tsx";
+import {useContext, useState } from "react";
 
 type DashboardHeaderProps = { tab: number };
 
 const DashboardHeader: React.FC<DashboardHeaderProps> = ({ tab }) => {
   const [isNotificationTabOpen, setNotificationTabOpen] = useState(false);
   const [isNotificationTabVisible, setNotificationTabVisible] = useState(false);
+  const authContext = useContext(AuthenticationContext);
+  const { firestoreUserDetails } = authContext as unknown as {firestoreUserDetails: any};
+  const isAdmin = firestoreUserDetails.role === 'admin';
 
   const toggleNotificationTab = () => {
     if (isNotificationTabOpen) {
@@ -20,11 +24,23 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({ tab }) => {
   };
 
   return (
-    <div className="bg-white w-full shadow-sm flex justify-between py-3 px-8">
+    <div className="bg-white w-full shadow-sm shadow-black/35 flex justify-between py-3 px-8">
       {tab === 5 ? <SearchBar /> : <div />}
 
       <div>
         <div className="flex justify-between items-center w-fit gap-8">
+          {isAdmin ?
+            <a
+              href="http://localhost:3000/admin"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="stroked rounded-full text-xs whitespace-nowrap px-4 py-2 text-primary border-2 border-primary hover:text-white hover:bg-primary"
+            >
+              CMS Panel
+            </a>
+            : <div/>
+          }
+
           <div className="flex">
             <span className="p-1 rounded-full bg-primary cursor-pointer shadow-lg transform active:scale-[80%] transition-transform" onClick={toggleNotificationTab}>
               <IoIosNotifications className="text-white text-3xl" />
