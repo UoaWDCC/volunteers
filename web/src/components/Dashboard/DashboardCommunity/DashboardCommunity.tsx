d8af3234e29d41df3d3eca3e3fbfbe15dee615b5
pche314 <Pche314@aucklanduni.ac.nz>
2025-08-25 22:52:12 +1200

    Finished implementing the community overflow issue #388

--- web/src/components/Dashboard/DashboardCommunity/DashboardCommunity.tsx
+++ web/src/components/Dashboard/DashboardCommunity/DashboardCommunity.tsx
@@ -1,11 +1,11 @@
-import CommunityGalleryWhole from "../DashboardCommunity/CommunityGalleryWhole";
+import CommunityGallery from "../DashboardCommunity/CommunityGallery";
 import Leaderboard from "../Leaderboard";
 
 function DashboardCommunity() {
   return (
     <div className="flex flex-row w-full h-full gap-5">
-      <div className="flex flex-col w-[75%] h-full overflow-y-auto scrollbar-none shadow-lg rounded-lg">
-        <CommunityGalleryWhole />
+      <div className="flex flex-col overflow-y-auto scrollbar-none">
+        <CommunityGallery />
       </div>
 
       <div className="w-[20%]">
